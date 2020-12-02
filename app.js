ae963e9b22e3f419b94b4e77f691eab44fbb4919
LucasSpenthof <30935656+LucasSpenthof@users.noreply.github.com>
2020-12-01 22:26:52 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const clienteController = require('./src/controllers/ClienteController')
+
+
+const app = express()
+const port = 3001
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.set('view engine','.html')
+
+nunjucks.configure('./src/views', {
+  autoescape: true,
+  express: app
+});
+
+app.get('/',(req,res)=>{
+  res.render('index')
+})
+app.get('/categoria-produto/listar',(req,res)=>{
+  let {categoriasDeProduto} = require('./src/db/fakeData')
+  res.render('categoria-produto/listar',{categorias:categoriasDeProduto})
+})
+app.get('/categoria-produto/adicionar',(req,res)=>{
+  res.render('categoria-produto/adicionar')
+})
+
+app.get('/cliente/listar',clienteController.index)
+app.get('/cliente/adicionar',clienteController.create)
+app.post('/cliente/salvar',clienteController.store)
+app.get('/cliente/editar/:id',clienteController.edit)
+app.post('/cliente/atualizar',clienteController.update)
+app.get('/cliente/excluir:id', clienteController.delete)
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
