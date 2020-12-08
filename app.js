363f88dd58504c11f616b3c27d098019ffe80169
LucasSpenthof <30935656+LucasSpenthof@users.noreply.github.com>
2020-12-07 22:03:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const clienteController = require('./src/controllers/ClienteController')
 
 
 const app = express()
-const port = 3001
+const port = 3000
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.set('view engine','.html')
@@ -17,20 +17,21 @@ nunjucks.configure('./src/views', {
 app.get('/',(req,res)=>{
   res.render('index')
 })
-app.get('/categoria-produto/listar',(req,res)=>{
-  let {categoriasDeProduto} = require('./src/db/fakeData')
-  res.render('categoria-produto/listar',{categorias:categoriasDeProduto})
-})
-app.get('/categoria-produto/adicionar',(req,res)=>{
-  res.render('categoria-produto/adicionar')
-})
+
+app.get('/categoria-produto/listar',categoriaProdutoController.index)
+app.get('/categoria-produto/adicionar',categoriaProdutoController.create)
+app.get('/categoria-produto/salvar',categoriaProdutoController.store)
+app.get('/categoria-produto/editar/:id',categoriaProdutoController.edit)
+app.post('/categoria-produto/atualizar',categoriaProdutoController.update)
+app.get('/categoria-produto/excluir/:id',categoriaProdutoController.delete)
+
 
 app.get('/cliente/listar',clienteController.index)
 app.get('/cliente/adicionar',clienteController.create)
 app.post('/cliente/salvar',clienteController.store)
 app.get('/cliente/editar/:id',clienteController.edit)
 app.post('/cliente/atualizar',clienteController.update)
-app.get('/cliente/excluir:id', clienteController.delete)
+app.get('/cliente/excluir/:id',clienteController.delete)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
